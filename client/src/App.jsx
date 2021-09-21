5dd6e83e1947037155f19ab9a7b9f7942b342bab
thewackyindian <thewackyindian@gmail.com>
2021-09-22 01:15:49 +0530

    Login/SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from './Navbar';
+import Login from './components/SignIn/Up/Login';
 
 function App() {
     return (
         <div>
-          <Navbar />  
+        <Login />
+        
         </div>
     )
 }
