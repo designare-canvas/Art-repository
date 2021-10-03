f60d17d1d5c0389481eea14b8e2f10f54a1c2d7a
gaur-abhi524 <abhiabhigaur524@gmail.com>
2021-10-03 19:05:00 +0530

    Add post area

--- client/src/Pages/Post/Post.jsx
+++ client/src/Pages/Post/Post.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import ShareIcon from '@mui/icons-material/Share';
-import Divider from '@mui/material/Divider';
-import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import Navbar from '../../components/navbar/Navbar';
-import InfoIcon from '@mui/icons-material/Info';
-import Button from '@mui/material/Button';
-import Comment from '../../components/comment/comment.jsx';
-import { IconButton } from '@material-ui/core';
-import Footer from '../../components/footer/footer'
+import Sidebar from "../../components/sidebar/sidebar";
+import Footer from '../../components/footer/footer';
+import Divider from "@mui/material/Divider";
+import PostArea from "../../components/postArea/postArea";
 import './Post.scss';
 
 function Post() {
@@ -17,69 +12,17 @@ function Post() {
         <>
             <Navbar />
             <div className="Post-page">
-                <div className="image-area">
-                </div>
-                <Divider orientation="vertical" flexItem />
-                <div className="sidebar">
-                    <div className="icons">
-                        <div>
-                        <IconButton aria-label="share">
-                            <ShareIcon  />
-                        </IconButton>
-                        <IconButton aria-label="save">
-                            <CreateNewFolderIcon  />
-                        </IconButton>
-                        <IconButton aria-label="favorite">
-                            <FavoriteIcon  />
-                        </IconButton>
-                        </div>
-                        <div>
-                            <Button variant="outlined" startIcon={<InfoIcon />} style={{color:"rgba(0,0,0,0.54)", borderColor:"rgba(0,0,0,0.54)"}} >
-                                Details
-                            </Button>
-                        </div>
-                    </div>
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                    <Comment imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
-                        name="daniel"
-                        comment="Nice work"
-                        timeAgo="17"
-                      />
-                </div>
+              <PostArea imgUrl="https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"
+                postTitle="first"
+                authorName="hsh"
+                postImgUrl="https://cdn.dribbble.com/users/1150809/screenshots/16485499/media/02e55b3a0d537c1335c05400518d5e45.jpg"
+                postDescription="Hello Designares ⚡
+                  Here is my new shot✌
+                  Hope you enjoyed it! ❤️ Thanks for your likes and comments! 💬
+                  Follow Me : IG | TW | BE Contact me: Email"
+               />
+              <Divider orientation="vertical" flexItem />
+              <Sidebar /> 
             </div>
             <Footer />
         </>
