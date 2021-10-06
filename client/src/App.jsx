a7611babea25569cea8259c1f9f91d935666c19f
tech-akash <techakash2000@gmail.com>
2021-10-06 11:07:14 +0530

    Filter Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./components/SignIn/signup";
 import Home from "../src/Pages/Home/Home.jsx";
 import Login from "./components/SignIn/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <Router>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from "../../components/navbar/Navbar";
 import Posts from "../../components/allPostsShow/ImageListShow";
+import Filter from '../../components/filter/filter';
+
 function Home() {
     return (
         <div>
             <Navbar />
+            <Filter />
             <Posts />
+
         </div>
     )
 }
