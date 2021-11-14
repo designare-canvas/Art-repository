80330ca7126e13536ed47db3f71c4446f520f79d
Raj Karan Singh <75250496+thewackyindian@users.noreply.github.com>
2021-11-15 00:04:54 +0530

    Imporvements (#30)
    * upload page and navbar
    
    * upload page and navbar
    
    * edit option added on posts

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -6,6 +6,8 @@ import Footer from "../../components/footer/footer";
 import axios from "axios";
 import Box from "@mui/material/Box";
 import { Typography } from "@material-ui/core";
+import bannerimg from './creativity.png';
+
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -37,7 +39,7 @@ function Home() {
 
     console.log(result);
     if (result.data.success) {
-      setPosts(shuffle(result.data.data).slice(1, 12 + 1));
+      setPosts(shuffle(result.data.data).slice(0, 12));
     }
   };
   useEffect(() => {
@@ -48,61 +50,76 @@ function Home() {
     <div className="home">
       <Navbar />
       <Box
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          alignItems: "center",
-          bgcolor: "background.paper",
-          overflow: "hidden",
-          borderRadius: "12px",
-          boxShadow: 1,
-          fontWeight: "bold",
-          width: "auto",
-          height: 300,
-          margin: "2%",
-          textAlign: "center",
-          backgroundColor: "#EEEEEE",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: { xs: "center", md: "flex-start" },
-            m: 3,
-            minWidth: { md: 350 },
-          }}
-        >
-          <Typography
-            variant="h1"
-            className="heading"
-            style={{
-              fontFamily: "Rajdhani",
-              fontStyle: "cursive",
-              fontSize: "1.85rem",
-              fontWeight: "700",
-              color: "#22577A",
-            }}
-          >
-            designare is the go-to resource for discovering <br /> and
-            connecting with designers and creative talent <br />
-            around the globe.
-            <br />
-            Dream , Depict , Develop
-          </Typography>
-        </Box>
-        <Box
-          component="img"
-          sx={{
-            height: 233,
-            width: 350,
-            maxHeight: { xs: 233, md: 167 },
-            maxWidth: { xs: 350, md: 250 },
-          }}
-          alt="Some design"
-          src=""
-        />
-      </Box>
+                sx={{
+                    display: 'flex',
+                    flexDirection: { xs: 'column', md: 'row' },
+                    alignItems: 'center',
+                    bgcolor: 'background.paper',
+                    overflow: 'hidden',
+                    borderRadius: '12px',
+                    boxShadow: 1,
+                    fontWeight: 'bold',
+                    width:"auto",
+                    height: 300,
+                    margin: "2%",
+                    textAlign:"center",
+                    backgroundColor: '#EEEEEE',
+                }}
+            >
+                 <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: { xs: 'center', md: 'flex-start' },
+                        m: 3,
+                        minWidth: { md: 350 },
+                    }}
+                >
+                <Typography
+                    variant="h1"
+                    className="heading"
+                    style={{
+                    fontFamily: "Rajdhani",
+                    fontStyle:"cursive",
+                    fontSize: "1.75rem",
+                    fontWeight: "500",
+                    color: "#22577A",
+                    }}
+                >
+                    <i style={{ fontSize: "4rem", fontFamily: "Allison"}}> Designare </i> is the go-to resource for discovering and <br />   connecting with designers
+                     and creative talent  around<br />around the globe.
+                    </Typography>
+                    <Typography
+                    variant="h1"
+                    className="heading"
+                    style={{
+                    fontFamily: "Josefin Sans",
+                    fontStyle:"cursive",
+                    fontSize: "2.5rem",
+                    fontWeight: "800",
+                    lineHeight: 1.5,
+                    color: "#22577A",
+                    margin:"4% 0% 0% 10%"
+                    }}
+                >
+                    Dream , Depict  , Develop
+                    </Typography>
+                   
+                </Box>
+                <Box
+                    component="img"
+                    sx={{
+                        height: 233,
+                        width: 350,
+                        maxHeight: { xs: 233, md: 167 },
+                        maxWidth: { xs: 350, md: 250 },
+                        marginLeft:"20%"
+                    }}
+                    alt="Some design"
+                    src={bannerimg} 
+                />
+               
+            </Box>
       <Filter shuffle={shuffle} setPosts={setPosts} />
       <Posts posts={posts} />
       <Footer />
